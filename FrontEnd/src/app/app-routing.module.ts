f495e63e67eb7367a14c7ad6af9e9be20cad6aba
Joshefs-Rubio <almn.joshefsrubio@cbtis66.edu.mx>
2024-10-20 18:04:36 -0600

    CSS dashboard

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuInicioComponent } from './menu-inicio/menu-inicio.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ModeloYoungComponent } from './modelo-young/modelo-young.component';
+import { LeyHookeComponent } from './ley-hooke/ley-hooke.component';
+import { PrincipioPascalComponent } from './principio-pascal/principio-pascal.component';
+
+
 const routes: Routes = [
   { path: '', redirectTo: '/menuInicio', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'menuInicio', component: MenuInicioComponent }
+  { path: 'menuInicio', component: MenuInicioComponent },
+  { path: 'modelo_young', component: ModeloYoungComponent },
+  { path: 'ley_hooke', component: LeyHookeComponent },
+  { path: 'principio_pascal', component: PrincipioPascalComponent }
 ];
 
 @NgModule({
--- FrontEnd/src/app/dashboard/dashboard.component.ts
+++ FrontEnd/src/app/dashboard/dashboard.component.ts
@@ -5,50 +5,6 @@ import { Component, AfterViewInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements AfterViewInit {
-  currentIndex = 0;
-  slider: HTMLElement | null = null;
-  cards: NodeListOf<HTMLElement> | undefined;
-
-  ngOnInit(): void {
-    window.scrollTo(0, 0);
-   }
-   
-  ngAfterViewInit() {
-    this.slider = document.getElementById('slider');
-    this.cards = document.querySelectorAll('.card');
-
-    const prevBtn = document.getElementById('prevBtn');
-    const nextBtn = document.getElementById('nextBtn');
-
-    if (prevBtn && nextBtn) {
-      prevBtn.addEventListener('click', () => this.moveToPrev());
-      nextBtn.addEventListener('click', () => this.moveToNext());
-    }
-    this.updateSlider(this.currentIndex);
-  }
-
-  updateSlider(index: number) {
-    if (this.slider) {
-      const offset = -index * 20; // Desplazar el slider horizontalmente por cada tarjeta
-      this.slider.style.transform = `translateX(${offset}%)`;
-    }
-
-    this.cards?.forEach(card => card.classList.remove('active'));
-    this.cards?.[index]?.classList.add('active');
-  }
-
-  moveToPrev() {
-    if (this.currentIndex > 0) {
-      this.currentIndex--;
-      this.updateSlider(this.currentIndex);
-    }
-  }
-
-  moveToNext() {
-    if (this.cards && this.currentIndex < this.cards.length - 1) {
-      this.currentIndex++;
-      this.updateSlider(this.currentIndex);
-    }
-  }
+export class DashboardComponent {
+  
 }
